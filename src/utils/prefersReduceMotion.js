21d006dea775507a21029f159be87d15cd66a390
Matteo Badini <matteo.badini95@gmail.com>
2019-04-27 19:56:14 +0200

    feat: add prefers-reduce-motion check
    disable animation when prefers-reduce-motion is enable

---
+++ src/utils/prefersReduceMotion.js
@@ -0,0 +1,12 @@
+/**
+ * Returns:
+ * true if reduce motion is active;
+ * false otherwise.
+ *
+ * @return {boolean}
+ */
+function prefersReduceMotion() {
+	return !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+}
+
+export default prefersReduceMotion
